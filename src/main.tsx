c61bfddcd0b337c80b9e9685f608229b38f12ed5
sa'd <asaad5436@gmail.com>
2024-05-29 02:04:16 +0500

    style: add borders to the main container

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,10 @@ import { ModeToggle } from "./components/mode-toggle"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App>
-        <div className='flex justify-center py-2 w-full h-[800px]'>
-            <ModeToggle/>
+        <div className='flex flex-col w-full h-[900px] p-4 items-center justify-between'>
+             <ModeToggle/>
+            <div className='flex justify-center py-2 w-1/2 h-[90%] border-border border rounded-md'>
+            </div>
         </div>
     </App>
   </React.StrictMode>,
