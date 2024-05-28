0f0d17b7cf76994eb01f3bf40dfe78780a236ba4
sa'd <asaad5436@gmail.com>
2024-05-29 00:42:52 +0500

    feat: add themes, and theme toggle button

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ModeToggle } from "./components/mode-toggle"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App>
+        <div className='flex justify-center py-2 w-full h-[800px]'>
+            <ModeToggle/>
+        </div>
+    </App>
   </React.StrictMode>,
 )
